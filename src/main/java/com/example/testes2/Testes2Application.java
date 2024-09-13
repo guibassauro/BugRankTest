5c0e22ccd07ce6d91ed8659a672f6edb644ba979
Guilherme Barbian <guiabarbian2@gmail.com>
2024-09-12 21:13:48 -0300

    testes em selenium feitos

---
+++ src/main/java/com/example/testes2/Testes2Application.java
@@ -0,0 +1,13 @@
+package com.example.testes2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testes2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testes2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testes2/Testes2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testes2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testes2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
